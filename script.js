806cf8799b7d19601b9f7eb5e5c8d35f3c03dd0e
mlody-garage <dispensercontact@proton.me>
2024-06-07 08:37:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const music = document.getElementById('music');
-    if (music) {
-        music.addEventListener('canplay', function() {
-            music.play();
-        });
-
-        document.addEventListener('click', function() {
-            music.play();
-            document.removeEventListener('click', arguments.callee);
-        });
-    }
-
-    if (document.getElementById('map')) {
-        initMap();
-    }
-
-    // scrollToHome(); // Opcjonalnie, jeśli chcesz zachować tę funkcjonalność
-
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navLinks = document.querySelector('.nav-links');
-
-    if (menuToggle) {
-        menuToggle.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-        });
-
-        // Obsługa dotknięcia na urządzeniach mobilnych
-        menuToggle.addEventListener('touchstart', () => {
-            navLinks.classList.toggle('active');
-        });
-    }
-
-    const header = document.querySelector('header');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 128) {
-            header.classList.add('scrolled');
-        } else {
-            header.classList.remove('scrolled');
-        }
+document.addEventListener('DOMContentLoaded', () => {
+  const music = document.getElementById('music');
+  if (music) {
+    music.addEventListener('canplay', music.play);
+    document.addEventListener('click', () => {
+      music.play();
+      document.removeEventListener('click', arguments.callee);
     });
+  }
+
+  if (document.getElementById('map')) {
+    initMap();
+  }
+
+
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  if (menuToggle) {
+    menuToggle.addEventListener('click', () => navLinks.classList.toggle('active'));
+    menuToggle.addEventListener('touchstart', () => navLinks.classList.toggle('active'));
+  }
+
+  const header = document.querySelector('header');
+  window.addEventListener('scroll', () => {
+    header.classList.toggle('scrolled', window.scrollY > 128);
+  });
 });
 
 function initMap() {
-    const map = L.map('map').setView([50.34484076841697, 20.403919425863336], 15);
+  const map = L.map('map').setView([50.34484076841697, 20.403919425863336], 15);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([50.34484076841697, 20.403919425863336], {
-        alt: "Młody Garage - Drożejowice 110/5, Skalbmierz 28-530"
-    }).addTo(map)
-       .bindPopup('Młody Garage<br>Drożejowice 110/5, Skalbmierz 28-530')
-       .openPopup();
+  L.marker([50.34484076841697, 20.403919425863336], {
+    alt: "Młody Garage - Drożejowice 110/5, Skalbmierz 28-530"
+  }).addTo(map)
+   .bindPopup('Młody Garage<br>Drożejowice 110/5, Skalbmierz 28-530')
+   .openPopup();
 }
